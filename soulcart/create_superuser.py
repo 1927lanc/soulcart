ce14c7d9b4cb189ee884f427665ca05137ba2589
1927lanc <1927lanc@gmail.com>
2026-03-10 22:11:07 +0530

    Auto-create superuser on deploy

---
+++ soulcart/create_superuser.py
@@ -0,0 +1,12 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soulcart.settings')
+django.setup()
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@soulcart.com', 'Admin@1234')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+
