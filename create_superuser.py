0b1730d0da320052149fdca17d04aec27349ff35
1927lanc <1927lanc@gmail.com>
2026-03-10 22:29:55 +0530

    Fix create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,4 +1,3 @@
-Set-Content -Path "create_superuser.py" -Value @"
 import os
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soulcart.settings')
@@ -6,7 +5,7 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-User.objects.create_superuser('admin', 'admin@soulcart.com', 'Admin@1234')
-print('Superuser created!')
+    User.objects.create_superuser('admin', 'admin@soulcart.com', 'Admin@1234')
+    print('Superuser created!')
 else:
-print('Superuser already exists.')
+    print('Superuser already exists.')
