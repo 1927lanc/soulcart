c4c98fe972bb85d192395d4dc794a8b6a8567b5c
1927lanc <1927lanc@gmail.com>
2026-03-11 17:21:35 +0530

    Fix build.sh remove duplicate

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata products_data.json
 python create_superuser.py
-python manage.py loaddata products_data.json
