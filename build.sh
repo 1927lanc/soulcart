999f48105afca1916d9f9cb94b0f151ae90429a3
1927lanc <1927lanc@gmail.com>
2026-03-11 17:12:45 +0530

    Add products fixture and load on deploy

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata products_data.json
 python create_superuser.py
+python manage.py loaddata products_data.json
