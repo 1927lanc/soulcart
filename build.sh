581db230596f1bc362fada7510dc3773e22f2e47
1927lanc <1927lanc@gmail.com>
2026-03-14 13:40:12 +0530

    Fix production build script

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
  #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files for Django Admin and other static assets
 python manage.py collectstatic --noinput
-
-# Apply database migrations
 python manage.py migrate
